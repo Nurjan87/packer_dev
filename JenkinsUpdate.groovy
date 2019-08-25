3c0fca7f9aebcef85c43bafadeab68e1ac8a53d2
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-25 17:05:57 -0500

    test

--- JenkinsUpdate.groovy
+++ JenkinsUpdate.groovy
@@ -56,11 +56,22 @@ pipeline{
                         }
                     }
                 }
+                stage("Filter AMI"){
+                    steps{
+                        scripts{
+                            def AMI
+                            if (REGION == "us-east-1") {
+                                AMI = "ami-0b898040803850657"
+                            } else if (REGION == "us-east-2"){
+                                 AMI = "ami-0d8f6eb4f641ef69
+                            }       
+                    }
+                }
             }
         }
         stage("Build Image"){
             steps{
-                sh 'packer build -var "region=${REGION}" updated/updated.json'
+                sh 'packer build -var "region=${REGION}" -var "AMI=${AMI}" updated/updated.json'
                 echo "Hello"
             }
         }
